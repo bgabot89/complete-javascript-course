a2b0e73794cc2443ba74dc85669c29c51c77dcd0
Brian Gabot <bgabot89@gmail.com>
2024-06-21 10:00:39 -0700

    on arrays

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -27,34 +27,276 @@
 
 //////*Values vs References*////
 
-const flight = 'LH234'; //primitive value
-const person = {
-  name: 'bob ross',
-  passport: 1234567890,
+// const flight = 'LH234'; //primitive value
+// const person = {
+//   name: 'bob ross',
+//   passport: 1234567890,
+// };
+
+// const checkIn = function (flightNumber, passenger) {
+//   flightNumber = 'SS333';
+//   passenger.name = 'Mr. ' + passenger.name; //manipulating object in the memory heap
+
+//   if (passenger.passport === 1234567890) {
+//     // alert('Check in');
+//     console.log('Check in');
+//   } else {
+//     // alert('Wrong Passport');
+//     console.log('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, person);
+// console.log(flight);
+// console.log(person);
+
+// //passing primitive value, creates a copy and stored in memory. Passing in an object, will manipulate the original
+
+// const newPassport = function (person) {
+//   person.passport = Math.round(Math.random() * 10000000);
+// };
+
+// newPassport(person);
+// checkIn(flight, person);
+
+//////*Accepting Callback Functions*////
+
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// //Higher order function, since it takes in another function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('kS is the best', upperFirstWord);
+
+// transformer('JS is the best', oneWord);
+
+// //JS uses callbacks all the time
+// const high5 = function () {
+//   console.log('👍');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// ['lei', 'bob', 'thomas'].forEach(high5);
+
+//callbacks are useful because makes easy to split up code and resuable. Allows to create obstruction: hide details of some code implementation
+
+//////*Functions returning Functions*////
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+
+// greeterHey('Jonas');
+
+// greet('hi')('bob');
+
+//arrow function method
+
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// // greet('hello', 'bob');
+
+// greet('hi')('bob');
+
+//////*Call and Apply Method*////
+
+// const zipair = {
+//   airline: 'zipAir',
+//   iataCode: 'ZP',
+//   bookings: [],
+//   book: function (flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode} ${flightNum}`, name });
+//   },
+// };
+
+// zipair.book(1234, 'Brian Gabot');
+// zipair.book(2345, 'Rob Schneider');
+// console.log(zipair);
+
+// const unitedair = {
+//   airline: 'United Airline',
+//   iataCode: 'UN',
+//   bookings: [],
+// };
+
+// const book = zipair.book;
+// //Does not work
+// // book(23, 'Sarah Williamns');
+
+// //CALL METHOD
+
+// book.call(unitedair, 23, 'Sarah Williams');
+// console.log(unitedair);
+
+// book.call(zipair, 238, 'Mary Blige');
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// book.call(swiss, 238, 'Tom Boos');
+
+// //APPLY method, not used a lot in modern JS
+// const flightData = [582, 'George Coopeer'];
+// book.apply(swiss, flightData);
+
+// //call method, used more often
+// book.call(swiss, ...flightData);
+// console.log(swiss);
+
+// //////*BIND Method, manually set this keyword for any function called. Returns a new function with new this*////
+
+// const bookUnited = book.bind(unitedair);
+// const bookJP = book.bind(zipair);
+// const bookLX = book.bind(swiss);
+
+// bookUnited(23, 'Steven Williams');
+
+// const bookEW23 = book.bind(unitedair, 23);
+// bookEW23('Jonas BB');
+// bookEW23('Martha Cooper');
+
+// //With Event Listeners
+// zipair.planes = 300;
+// zipair.buyPlane = function () {
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// const buyButton = document.querySelector('.buy');
+// buyButton.addEventListener('click', zipair.buyPlane.bind(zipair));
+
+// //Partial application
+
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// // addVAT = value => value + value *0.23;
+
+// console.log(addVAT(100));
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));
+
+// //////*Immediately Invoked Functions*////
+
+const runOnce = function () {
+  console.log('Run once and then never again');
 };
 
-const checkIn = function (flightNumber, passenger) {
-  flightNumber = 'SS333';
-  passenger.name = 'Mr. ' + passenger.name; //manipulating object in the memory heap
+runOnce();
 
-  if (passenger.passport === 1234567890) {
-    // alert('Check in');
-    console.log('Check in');
-  } else {
-    // alert('Wrong Passport');
-    console.log('Wrong passport');
-  }
+// IIFE
+(function () {
+  console.log('This will never run again');
+})();
+
+(() => console.log('This will also never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+
+////////*Closure Functions*////
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
 };
 
-checkIn(flight, person);
-console.log(flight);
-console.log(person);
+const booker = secureBooking();
 
-//passing primitive value, creates a copy and stored in memory. Passing in an object, will manipulate the original
+booker();
+booker();
+booker();
 
-const newPassport = function (person) {
-  person.passport = Math.round(Math.random() * 10000000);
+console.dir(booker);
+
+//closures remembers all the variables that existed, at the functions birthplace
+//A function has access to the variable environment (VE) of the execution context in which it was created, even after execution context is gone
+/*A closure is the closed-over variable environment of the execution context in which a function was created, even after that 
+execution context is gone.
+
+/*A closure gives a function access to all the variables of its parent function, even after that parent function has returned.
+The function keeps a reference to its outer scope, which preserves the scope chain throughout time.*/
+
+/*A closure makes sure that a function doesn't loose connection to variables that existed at the functions birthplace*/
+
+//Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
 };
 
-newPassport(person);
-checkIn(flight, person);
+//Re assigning  f function
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 3);
+  };
+};
+
+g();
+f();
+
+//Re-assigning f function
+h();
+f();
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+
+boardPassengers(180, 3);
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,232 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const createUserNames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+    // return username;
+  });
+};
+
+// const user = 'Steven Thomas Williams'; //stw
+
+console.log(createUserNames(accounts));
+
+console.log(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+/////////////////////////////////////////////////
+
+// //SLICE, creates new array does not mutate original
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// console.log(arr.slice(2, 4));
+
+// console.log(arr.slice(-2));
+
+// console.log(arr.slice(-1));
+
+// console.log(arr.slice(1, -1));
+
+// console.log(arr.slice());
+
+// console.log([...arr]);
+
+// //SPLICE, mutates the original
+
+// // console.log(arr.splice(2));
+
+// // arr.splice(-1);
+
+// console.log(arr);
+
+// //REVERSE, mutates the original array
+// arr = ['a', 'b', 'c', 'd', 'e'];
+
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+
+// //CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// console.log(...arr, ...arr2);
+
+// //JOIN
+// console.log(letters.join(' - '));
+
+// //Array at METHOD
+// const arr3 = [23, 11, 64];
+// console.log(arr3[0]);
+// console.log(arr3.at(0));
+
+// //getting last array method
+// console.log(arr3[arr3.length - 1]);
+// console.log(arr3.slice(-1)[0]);
+// console.log(arr3.at(-1));
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log('--for EACH');
+// movements.forEach(function (movement, i, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// });
+
+// //0: function(200)
+// //1: function(450)
+// //2: function(400)
+
+// //Map
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// //Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, key, map) {
+//   console.log(`${key}:${value}`);
+// });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, 
+and stored the data into an array (one array for each). For now, they are 
+just interested in knowing whether a dog is an adult or a puppy. 
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), 
+and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create 
+a shallow copy of Julia's array, and remove the cat ages from that copied array 
+(because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// const dogsJulia = [3, 5, 2, 12, 7];
+// const dogsKate = [4, 1, 15, 8, 3];
+
+// const checkDogs = function (juliadogs, katedogs) {
+//   const juliaDogsCorrected = juliadogs.slice();
+//   juliaDogsCorrected.splice(0, 1);
+//   juliaDogsCorrected.splice(-2);
+//   console.log(juliaDogsCorrected);
+
+//   const dogs = juliaDogsCorrected.concat(dogsKate);
+
+//   console.log(dogs);
+
+//("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+//   dogs.forEach(function (element, index) {
+//     if (element >= 3) {
+//       console.log(
+//         `Dog number ${index + 1} is an adult, and is ${element} years old`
+//       );
+//     } else {
+//       console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+//     }
+//   });
+// };
+
+// checkDogs(dogsJulia, dogsKate);
+
+//MAP Method, returns a new array, containing the results of an operation
+//Example
+// const arrM = [3, 1, 4, 3, 2];
+
+// const newMap = arrM.map(x => x * 2);
+
+// console.log(newMap);
+
+//Filter Method, filter returns a new array containing the array elements that passed a specified condition
+//Example
+// const filtArr = [3, 1, 4, 3, 2];
+
+// const newFilter = filtArr.filter(num => num > 2);
+
+// console.log(newFilter);
+
+//Reduce Method, "reduces" all array elements down to one single value (e.g. adding all elements together), acc+current
+// const array1 = [1, 2, 3, 4];
+
+// const initialValue = 0;
+// const sumWithInitial = array1.reduce(
+//   (acc, currValue) => acc + currValue,
+//   initialValue
+// );
+
+// console.log(sumWithInitial);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+const eurtoUSD = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurtoUSD;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurtoUSD);
+
+const movementsDescip = movements.map(
+  (movement, i) =>
+    `Movement ${i + 1}: You ${
+      movement > 0 ? 'deposited' : 'withdrew'
+    } ${Math.abs(movement)}`
+);
+
+console.log(movementsDescip);
